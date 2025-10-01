68ee6ed97134e319ba42e0622807771e8d067c37
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2025-10-01 11:55:18 -0300

    creating feed

--- src/app/components/structures/SearchFormResults.tsx
+++ src/app/components/structures/SearchFormResults.tsx
@@ -23,7 +23,7 @@ export default function SearchFormResults({profiles}: Props) {
                 <form action="" className="">
                     <h2 className="text-center font-bold font-second text-3xl mb-5">¡Encontrá personas maravillosas!</h2>
                     <label htmlFor="" className="w-full h-15 rounded-2xl border border-slate-400 bg-slate-50 flex justify-between items-center px-5">
-                        <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} name="search" placeholder="Buscar..." className="w-full text-xl rounded-lg h-1/2 focus:outline-none focus:ring-0" />
+                        <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} name="search" placeholder="Escribe para comenzar a buscar..." className="w-full text-xl rounded-lg h-1/2 focus:outline-none focus:ring-0" />
                         <button type="submit" className="cursor-pointer hover:scale-110 active:scale-110 transition"><Search /></button>
                     </label>
                 </form>
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,4 @@
+import CreatePost from "../components/structures/CreatePost";
 import { fetchUserById } from "../lib/data-server";
 import { createClient } from "../utils/supabase/server";
 
@@ -11,12 +12,16 @@ export default async function page() {
         // redirect("/404")
     }    
     const profile = data.user ? await fetchUserById(data.user.id) : null;
+    const id = data.user?.id
     console.log(profile);
     
     return (
         profile ? (
             <main className="flex items-center justify-center w-full h-[600px]">
-                <h2 className="font-second font-semibold">Bienvenid@, <span className="bg-yellow-main text-black-main px-3 py-1 rounded">{profile?.name}</span></h2>
+                <div className="w-1/2 flex flex-col items-center gap-10">
+                    <CreatePost id={id} />
+                    <h2 className="font-second font-semibold">Bienvenid@, <span className="bg-yellow-main text-black-main px-3 py-1 rounded">{profile?.name}</span></h2>
+                </div>
             </main>
         ) : (
             <main className="flex items-center justify-center w-full h-[600px]">
