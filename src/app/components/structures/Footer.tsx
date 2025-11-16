737d7e7dc8ec8224643e329cb3019f11d31c5e2e
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2025-11-16 17:17:04 -0300

    created star spin in home page

--- src/app/components/structures/Footer.tsx
+++ src/app/components/structures/Footer.tsx
@@ -11,14 +11,14 @@ export default function Footer() {
                 </Link>
                 <div className="flex flex-col md:flex-row flex-wrap justify-start gap-10 md:gap-56">
                     <div>
-                        <h3 className="font-medium text-2xl mb-4">Info.</h3>
+                        <h3 className="font-medium text-2xl mb-4">Empezar</h3>
                         <ul className="flex flex-col gap-3">
                             <Link href="/" className="hover:bg-black-main hover:text-yellow-main active:bg-black-main active:text-yellow-main px-2 rounded">Inicio</Link>
                             <Link href="/home" className="hover:bg-black-main hover:text-yellow-main active:bg-black-main active:text-yellow-main px-2 rounded">Feed</Link>
                         </ul>
                     </div>
                     <div>
-                        <h3 className="font-medium text-2xl mb-4">Info.</h3>
+                        <h3 className="font-medium text-2xl mb-4">Busquedas</h3>
                         <ul className="flex flex-col gap-3">
                             <Link href="/search/profiles" className="hover:bg-black-main hover:text-yellow-main active:bg-black-main active:text-yellow-main px-2 rounded">Buscar Perfiles</Link>
                             <Link href="/search/documents" className="hover:bg-black-main hover:text-yellow-main active:bg-black-main active:text-yellow-main px-2 rounded">Buscar Documentos</Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function page() {
                     <p className="w-full md:w-2/4 text-start md:text-center font-medium px-5"><i>&quot;Hive&quot;</i> es una plataforma orientada a la comunidad universitaria de la UNLaR, creada con el proposito de brindar un espacio de repositorio para publicar documentos, trabajos prácticos o cualquier tipo de producción, con el fin de difundirlos.</p>
                     <hr className="border w-20 md:hidden block" />
                     <p className="font-medium text-wrap px-7 leading-9">Creado por <span className="text-yellow-main text-nowrap bg-black-main rounded-lg px-2 py-1">Ignacio Joaquín Pantoja</span> y <span className="text-yellow-main text-nowrap bg-black-main rounded-lg px-2 py-1">Paula Fuentes</span></p>
-                    <Link href="/home" className="ml-7 mt-2 md:ml-0 self-start md:self-center hover:text-black-main hover:bg-yellow-main border transition text-yellow-main bg-black-main rounded-lg px-3 py-2 flex items-center gap-2"><Star />Comenzá a Explorar</Link>
+                    <Link href="/home" className="group ml-7 mt-2 md:ml-0 self-start md:self-center hover:text-black-main hover:bg-yellow-main border transition text-yellow-main bg-black-main rounded-lg px-3 py-2 flex items-center gap-2"><Star className="star-icon" />Comenzá a Explorar</Link>
                 </div>
             </main>
             <footer className="w-full min-h-[600px] flex items-center justify-evenly text-yellow-main" style={{ backgroundColor: "#121211" }}>
@@ -29,25 +29,25 @@ export default function page() {
                     </Link>
                     <div className="flex flex-col md:flex-row flex-wrap justify-start gap-10 md:gap-56">
                         <div>
-                            <h3 className="font-medium text-2xl mb-4">Info.</h3>
+                            <h3 className="font-medium text-2xl mb-4">Empezar</h3>
                             <ul className="flex flex-col gap-3">
-                                <Link href="/" className="hover:bg-black-main hover:text-yellow-main px-2 rounded">Inicio</Link>
-                                <Link href="/about-us" className="hover:bg-black-main hover:text-yellow-main px-2 rounded">Nosotros</Link>
-                                <Link href="/home" className="hover:bg-black-main hover:text-yellow-main px-2 rounded">Feed</Link>
+                                <Link href="/" className="hover:bg-black-main hover:text-yellow-main active:bg-black-main active:text-yellow-main px-2 rounded">Inicio</Link>
+                                <Link href="/home" className="hover:bg-black-main hover:text-yellow-main active:bg-black-main active:text-yellow-main px-2 rounded">Feed</Link>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3 className="font-medium text-2xl mb-4">Busquedas</h3>
+                            <ul className="flex flex-col gap-3">
+                                <Link href="/search/profiles" className="hover:bg-black-main hover:text-yellow-main active:bg-black-main active:text-yellow-main px-2 rounded">Buscar Perfiles</Link>
+                                <Link href="/search/documents" className="hover:bg-black-main hover:text-yellow-main active:bg-black-main active:text-yellow-main px-2 rounded">Buscar Documentos</Link>
                             </ul>
                         </div>
                         <div>
                             <h3 className="font-medium text-2xl mb-4">Info.</h3>
                             <ul className="flex flex-col gap-3">
-                                <Link href="/search/profiles" className="hover:bg-black-main hover:text-yellow-main px-2 rounded">Buscar Perfiles</Link>
-                                <Link href="/search/documents" className="hover:bg-black-main hover:text-yellow-main px-2 rounded">Buscar Documentos</Link>
-                                <Link href="/donate" className="hover:bg-black-main hover:text-yellow-main px-2 rounded">Donaciones</Link>
-                            </ul>
-                        </div>
-                        <div>
-                            <h3 className="font-medium text-2xl mb-4">Info.</h3>
-                            <ul className="flex flex-col gap-3">
-                                <Link href="/terms" className="hover:bg-black-main hover:text-yellow-main px-2 rounded">Términos y Condiciones</Link>
+                                <Link href="/about-us" className="hover:bg-black-main hover:text-yellow-main active:bg-black-main active:text-yellow-main px-2 rounded">Nosotros</Link>
+                                <Link href="/donate" className="hover:bg-black-main hover:text-yellow-main active:bg-black-main active:text-yellow-main px-2 rounded">Donaciones</Link>
+                                <Link href="/terms" className="hover:bg-black-main hover:text-yellow-main active:bg-black-main active:text-yellow-main px-2 rounded">Términos y Condiciones</Link>
                             </ul>
                         </div>
                     </div>
